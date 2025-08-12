14247f126c98caaff0d40e57147d2b79af9f70df
keulma <jjyoung0403@naver.com>
2025-08-12 13:14:06 +0900

    feature:지도 기능 구현
    지도 출력, 마커 표시&클러스터링
    
    firestore 연동 후 지도 ui, 마커 이미지 변경 예정

---
+++ src/app/map/page.tsx
@@ -0,0 +1,9 @@
+import LostItemMap from '@/components/map/LostItemMap';
+
+export default function MapPage() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <LostItemMap />
+    </div>
+  );
+}
