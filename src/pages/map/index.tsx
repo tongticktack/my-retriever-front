9836e48943569326475a25b636407c3a318c3f72
tongticktack <provemyself99@gmail.com>
2025-08-13 09:05:44 +0900

    Feat. Panel 컴포넌트 전 페이지 적용

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,14 +1,17 @@
+import Panel from "@/components/Panel";
 import styles from "./map.module.css";
 
 export default function MapPage() {
   return (
     <main className={styles.main}>
-      <div className={styles.header}>
-        <h1 className={styles.title}>지도</h1>
-      </div>
-      <div className={styles.content}>
-        <p className={styles.description}>지도 기능이 곧 추가됩니다.</p>
-      </div>
+      <Panel>
+        <div className={styles.header}>
+          <h1 className={styles.title}>지도</h1>
+        </div>
+        <div className={styles.content}>
+          <p className={styles.description}>지도 기능이 곧 추가됩니다.</p>
+        </div>
+      </Panel>
     </main>
   );
 }
