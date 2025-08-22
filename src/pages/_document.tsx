f549f6294a7379b927a91cd982079400316c0747
tongticktack <provemyself99@gmail.com>
2025-08-22 17:29:01 +0900

    fix: update package.json and clean up _document.tsx
    - Added a missing comma in package.json for react-kakao-maps-sdk dependency.
    - Removed redundant Head component in _document.tsx to streamline the document structure.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,6 @@ export default function Document() {
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false&libraries=services,clusterer`}
           strategy="beforeInteractive"
         />
-      </Head>
-      <Head>
         <link rel="icon" href="/loosyChatBoxIcon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/loosyChatBoxIcon.svg" />
         <link rel="shortcut icon" href="/loosyChatBoxIcon.svg" />
