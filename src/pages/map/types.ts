05570013de8eb942a04eb2ca463617f9cfe79b49
keulma <jjyoung0403@naver.com>
2025-08-23 14:12:56 +0900

    fix.주석 수정

--- src/pages/map/types.ts
+++ src/pages/map/types.ts
@@ -11,11 +11,10 @@ export interface LostItem {
   storagePlace: string;
 }
 
-// '집단'을 나타내는 타입 정의 (클러스터링에 필요했던 key 속성 제거)
 export type RepresentativeMarker = {
   lat: number;
   lng: number;
-  items: LostItem[]; // 이 집단에 포함된 분실물 배열
+  items: LostItem[];
   id: string;
   storagePlace: string;
 };
